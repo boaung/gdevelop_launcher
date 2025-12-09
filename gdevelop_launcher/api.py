be573e402d05afb982f6b55f4a6114dfa058f044
Bo Aung <boaung@gmail.com>
2025-12-09 18:29:34 +0630

    Update api.py

--- gdevelop_launcher/api.py
+++ gdevelop_launcher/api.py
@@ -1,1 +1,17 @@
-# REPLACE_WITH_REAL_API_FROM_CANVAS
+# gdevelop_launcher/gdevelop_launcher/api.py
+import frappe
+
+
+@frappe.whitelist(allow_guest=True)
+def list_games():
+"""Return list of published games: title, slug, index_url"""
+games = frappe.get_all('GDevelop Game', filters={'status':'Published'}, fields=['name','title','slug','index_url'])
+return games
+
+
+@frappe.whitelist()
+def publish_game(name):
+doc = frappe.get_doc('GDevelop Game', name)
+doc.status = 'Published'
+doc.save()
+return doc

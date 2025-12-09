c31ca9c014ade818e25206d3bab5736a4a16bb3c
Bo Aung <boaung@gmail.com>
2025-12-09 18:23:36 +0630

    Modify setup.py with new version and metadata
    Updated package version and added metadata.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name="gdevelop_launcher",
-    version="1.0.0",
-    packages=find_packages(),
-    include_package_data=True,
+name="gdevelop_launcher",
+version="0.1",
+description="GDevelop Game Launcher for Frappe",
+author="Bo Aung",
+packages=find_packages(),
+zip_safe=False,
 )

dd502b5a7cb66b7902ae7126ff452fbe23ec9f40
Bo Aung <boaung@gmail.com>
2025-12-09 18:30:12 +0630

    Add upload helper function for GDevelop
    Implemented a simple upload helper function for GDevelop.

--- gdevelop_launcher/public/js/gdevelop_uploader.js
+++ gdevelop_launcher/public/js/gdevelop_uploader.js
@@ -1,1 +1,16 @@
-// REPLACE_WITH_REAL_JS_FROM_CANVAS
+// simple upload helper for Desk or Website
+function gdevelop_upload(form) {
+var data = new FormData(form);
+fetch('/api/resource/GDevelop%20Game', {
+method: 'POST',
+body: data,
+credentials: 'include'
+}).then(r=>r.json()).then(resp=>{
+if (resp.data) {
+alert('Game created: ' + resp.data.name + '. Please attach ZIP in the record and Save to deploy.');
+window.location = '/app/gdevelop-game/' + resp.data.name;
+} else {
+alert('Error: ' + JSON.stringify(resp));
+}
+})
+}
